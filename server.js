38a65622a53477314a7368093540900215866263
Zrotof <manduel21@gmail.com>
2020-05-14 14:18:34 +0200

    last try 2

--- server.js
+++ server.js
@@ -12,5 +12,6 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/personal-portfoli-angular/index.html'));
 });
 
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
